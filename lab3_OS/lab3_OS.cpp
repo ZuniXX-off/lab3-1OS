d30261ab4fe10e66a09956cad3bae45df5b622fc
ddima21012002@gmail.com <ddima21012002@gmail.com>
2021-11-27 22:43:15 +0300

    version-1.0

--- lab3_OS/lab3_OS.cpp
+++ lab3_OS/lab3_OS.cpp
@@ -1,7 +1,62 @@
 ﻿#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+
+DWORD WINAPI CalculationFunction(LPVOID lpParam);
+
+const uint64_t N = 100'000'000;
+const uint64_t blockSize = 930711*10;
+const long double invN = 1.0 / N;
+long double result = 0;
+uint64_t globalIterations = 0;
+HANDLE mutexHandle;
 
 int main()
 {
-    
+    system("chcp 1251");
+    HANDLE* myThreads;
+    int numThreads;
+    std::cout << " Введите количество потоков: ";
+    std::cin >> numThreads;
+    globalIterations = numThreads * blockSize;
+    mutexHandle = CreateMutex(NULL, FALSE, NULL);
+    myThreads = new HANDLE[numThreads];
+    for (int i = 0; i < numThreads; i++) {
+        myThreads[i] = CreateThread(NULL, 0, CalculationFunction, (LPVOID)i, CREATE_SUSPENDED, NULL);
+    }
+    uint64_t startTime = GetTickCount();
+    for (int i = 0; i < numThreads; i++)
+        ResumeThread(myThreads[i]);
+    WaitForMultipleObjects(numThreads, myThreads, TRUE, INFINITE);
+    uint64_t endTime = GetTickCount();
+    for (int i = 0; i < numThreads; i++)
+        CloseHandle(myThreads[i]);
+    CloseHandle(mutexHandle);
+    result *= invN;
+    std::cout << " Время вычислений: " << endTime - startTime << std::endl;
+    std::cout << " Результат работы: " << std::setprecision(15) << result;
+
+    delete[] myThreads;
+    myThreads = nullptr;
+
+    return 0;
+}
+
+DWORD WINAPI CalculationFunction(LPVOID lpParam) {
+    long double pi = 0, xi = 0;
+    uint64_t curIterations = ((uint64_t)lpParam) * blockSize;
+    while (curIterations < N) {
+        for (uint64_t i = curIterations; (i < curIterations + blockSize) && i < N; i++) {
+            xi = (i + 0.5) * invN;
+            pi += (4.0 / (1.0 + xi * xi));
+        }
+        curIterations = globalIterations;
+        WaitForSingleObject(mutexHandle, INFINITE);
+        globalIterations += blockSize;
+        result += pi;
+        ReleaseMutex(mutexHandle);
+        pi = 0;
+    }
+    return 1;
 }
 
