20de6f5eb1706b220abb4b3761c6b756b59db669
ddima21012002@gmail.com <ddima21012002@gmail.com>
2021-11-30 17:14:43 +0300

    -version 2.0 -with-atomic-calculations

--- lab3_OS/lab3_OS.cpp
+++ lab3_OS/lab3_OS.cpp
@@ -8,54 +8,61 @@ const uint64_t N = 100'000'000;
 const uint64_t blockSize = 110;
 const long double invN = 1.0 / N;
 long double result = 0;
-uint64_t globalIterations = 0;
-LPCRITICAL_SECTION bruhMoment = new CRITICAL_SECTION;
+std::atomic<unsigned> globalIterations = 0;
 
 int main()
 {
     system("chcp 1251");
     HANDLE* myThreads;
     int numThreads;
+    long double* threadCalcs;
+    
     std::cout << " Введите количество потоков: ";
+    
     std::cin >> numThreads;
-    globalIterations = numThreads * blockSize;
+    threadCalcs = new long double[numThreads];
     myThreads = new HANDLE[numThreads];
-    InitializeCriticalSection(bruhMoment);
+    
     for (int i = 0; i < numThreads; i++) {
-        myThreads[i] = CreateThread(NULL, 0, CalculationFunction, (LPVOID)i, CREATE_SUSPENDED, NULL);
+        threadCalcs[i] = 0;
+        myThreads[i] = CreateThread(NULL, 0, CalculationFunction, (LPVOID) (threadCalcs + i), CREATE_SUSPENDED, NULL);
     }
+    
     uint64_t startTime = GetTickCount();
-    for (int i = 0; i < numThreads; i++)
+    
+    for (int i = 0; i < numThreads; i++) {
         ResumeThread(myThreads[i]);
+    }
     WaitForMultipleObjects(numThreads, myThreads, TRUE, INFINITE);
+    
     uint64_t endTime = GetTickCount();
-    for (int i = 0; i < numThreads; i++)
+    
+    for (int i = 0; i < numThreads; i++) {
         CloseHandle(myThreads[i]);
-
+        result += threadCalcs[i];
+    }
+        
     result *= invN;
+
     std::cout << " Время вычислений: " << endTime - startTime << std::endl;
     std::cout << " Результат работы: " << std::setprecision(15) << result;
 
     delete[] myThreads;
     myThreads = nullptr;
-    DeleteCriticalSection(bruhMoment);
 
     return 0;
 }
 
 DWORD WINAPI CalculationFunction(LPVOID lpParam) {
     long double pi = 0, xi = 0;
-    uint64_t curIterations = ((uint64_t)lpParam) * blockSize;
+    uint64_t curIterations = globalIterations.fetch_add(1) * blockSize;
     while (curIterations < N) {
         for (uint64_t i = curIterations; (i < curIterations + blockSize) && i < N; i++) {
             xi = (i + 0.5) * invN;
             pi += (4.0 / (1.0 + xi * xi));
         }
-        EnterCriticalSection(bruhMoment);
-        curIterations = globalIterations;
-        globalIterations += blockSize;
-        result += pi;
-        LeaveCriticalSection(bruhMoment);
+        curIterations = globalIterations.fetch_add(1) * blockSize;
+        *((long double*) lpParam) += pi;
         pi = 0;
     }
     return 1;
